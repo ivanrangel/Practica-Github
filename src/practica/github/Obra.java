db9df7fd28dadf2c538b42d5527c32d540687d5a
Ivan Rangel <ivanete.rangel@gmail.com>
2012-11-09 11:32:08 -0430

    Creacion de Metodos Clase Obra

--- src/practica/github/Obra.java
+++ src/practica/github/Obra.java
@@ -12,5 +12,36 @@ public class Obra
 {
     private String Titulo;
     private Artista Autor;
-    private int anoEdicion;        
+    private int anoEdicion;    
+    
+    public void Obra(String titulo,int ano,Artista p ){
+      setTitulo(titulo);
+      setanoEdicion(ano);   
+      setAutor(p);      
+    }
+               
+    public void setTitulo(String titulo)
+    {
+        Titulo = titulo;
+    }
+    
+    public void setanoEdicion(int ano)
+    {
+        anoEdicion = ano;
+    }
+    
+    public void setAutor (Artista p)
+    {
+        Autor = p;
+    }       
+   
+    public String getTitulo()
+    {
+        return Titulo;
+    }
+    
+    public int getanoEdicion()
+    {
+        return anoEdicion;
+    }        
 }
