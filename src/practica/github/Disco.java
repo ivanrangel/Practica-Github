2086721817be05fd02de6aa23607db9197046f8c
jorge ramirez <ramirezhjorge@gmail.com>
2012-11-09 20:28:24 -0400

    Revert "Creación clase Disco y metodos de la clase Disco"
    This reverts commit bfaf434d8402aef027c93697c208356c57f85d92.

--- src/practica/github/Disco.java
+++ src/practica/github/Disco.java
@@ -10,47 +10,4 @@ package practica.github;
  */
 public class Disco {
     
-    private String discografica;
-    private int nCanciones;
-    //private String Titulo;
-    
-    public Disco (String titulo, Artista p,int ano, String discografia, int nCancion)
-    {
-      //setTitulo(titulo);
-      //setAutor(p);
-      //setanoEdicion(ano);   
-      setDiscografica(discografia);
-      setnCanciones(nCancion);
-    }    
-    
-    //public void setTitulo(String titulo) {
-    //Titulo = titulo;
-    //}
-	  
-    //public void setAuthor(Artista p) {
-    //Autor = p;
-    //}
-
-    //public void setYear(int year) {
-    // = year;
-    //}
-
-    public void setDiscografica(String discografia) {
-    this.discografica = discografia;
-    }
-
-    public void setnCanciones(int nCancion) {
-    this.nCanciones = nCancion;
-    }
-
-    public String getDiscografica() {
-    return discografica;
-    }
-
-    public int getnCanciones() {
-    return nCanciones;
-    }
-
-    
-    
 }
