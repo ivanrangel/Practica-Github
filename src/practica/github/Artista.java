16c09473956964aa53883c9229aac9dc220ce122
Ivan Rangel <ivanete.rangel@gmail.com>
2012-11-10 09:47:40 -0430

    Cambio de Archivo artista.txt a artista.java

--- src/practica/github/Artista.java
+++ src/practica/github/Artista.java
@@ -8,6 +8,33 @@ package practica.github;
  *
  * @author Administrador
  */
-public class Artista {
+public class Artista 
+{ 
+    private String Nombre; 
+    private int anoNacimiento; 
+
+    public void insertar(String nom,int ano)
+    {
+      setNombre(nom);
+      setanoNacimiento(ano);             
+    }
+
+    public void setNombre(String nom)
+    { 
+      Nombre=nom;    
+    }
+
+    public void setanoNacimiento(int ano)
+    {
+       anoNacimiento=ano;
+    }          
     
+    public String getNombre()
+    {
+        return Nombre; 
+    }
+    public int getanoNacimiento()
+    {
+        return anoNacimiento;
+    }      
 }
